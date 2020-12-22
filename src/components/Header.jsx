7aa8dfd5fd0f5cf75740b6943c3df0903416c0a7
Dhruv Kumar <56073146+dkumar-22@users.noreply.github.com>
2020-12-23 00:04:23 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HighlightIcon from '@material-ui/icons/Highlight';
+function Header() {
+  return (
+    <header>
+      <h1><HighlightIcon/> Keeper</h1>
+      <h6>Capture Your Memories</h6>
+    </header>
+  );
+}
+
+export default Header;
